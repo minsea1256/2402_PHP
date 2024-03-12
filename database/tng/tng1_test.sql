b8f2682114a4c1e85b53503762910361a497912e
minsea1256 <minsea1256@gmail.com>
2024-03-12 13:09:36 +0900

    240312_test

--- database/tng/tng1_test.sql
+++ database/tng/tng1_test.sql
@@ -13,7 +13,7 @@ VALUES (
 	,'Kwon'
 	,'minsea'
 	,'F'
-	,20240311
+	,DATE(NOW())
 );
 	
 -- 2. 월급, 직책, 소속부서 테이블에 자신의 정보를 적절하게 넣어주세요.
@@ -27,7 +27,7 @@ VALUES (
 	500000
 	,'신입'
 	,20240311
-	,99990311
+	,DATE(NOW())
 );
 
 INSERT INTO salaries (
@@ -69,7 +69,15 @@ VALUES (
 	,'Kim'
 	,'sowon'
 	,'F'
-	,20240311
+	,DATE(NOW())
+),
+VALUES (
+	500002
+	,19990103
+	,'Kim'
+	,'minsea'
+	,'F'
+	,DATE(NOW())
 );
 INSERT INTO titles (
 	emp_no
@@ -80,7 +88,13 @@ INSERT INTO titles (
 VALUES (
 	500001
 	,'Staff'
-	,20240311
+	,DATE(NOW())
+	,99990311
+),
+VALUES (
+	500002
+	,'Staff'
+	,DATE(NOW())
 	,99990311
 );
 
@@ -93,7 +107,13 @@ INSERT INTO salaries (
 VALUES (
 	500001
 	,2500000
-	,20240311
+	,DATE(NOW())
+	,99990311
+)
+,VALUES (
+	500002
+	,2500000
+	,DATE(NOW())
 	,99990311
 );
 INSERT INTO dept_emp (
@@ -105,99 +125,177 @@ INSERT INTO dept_emp (
 VALUES (
 	500001
 	,'d004'
-	,20240311
+	,DATE(NOW())
+	,99990311
+),
+VALUES (
+	500002
+	,'d004'
+	,DATE(NOW())
 	,99990311
 );
 -- 4. 나와 짝궁의 소속 부서를 d009로 변경해 주세요.
 UPDATE dept_emp
-SET
-	dept_no = 'd009'
-WHERE
-	emp_no = 500001
-;
+SET to_date = DATE(NOW())
+WHERE emp_no IN (500000,500001,500002);
+INSERT INTO dept_emp (
+	emp_no
+	,dept_no
+	,from_date
+	,to_date
+)
+VALUES (
+	500000
+	,'d009'
+	,20240311
+	,99990311
+),
+VALUES (
+	500001
+	,'d004'
+	,DATE(NOW())
+	,99990311
+),
+VALUES (
+	500002
+	,'d004'
+	,DATE(NOW())
+	,99990311
+);
 -- 5. 짝궁의 모든 정보를 삭제해 주세요.
 DELETE FROM employees
-WHERE emp_no = 500001
+WHERE emp_no IN (500001,500002) 
 ;
 DELETE FROM titles
-WHERE emp_no = 500001
+WHERE emp_no IN (500001,500002) 
 ;
 DELETE FROM salaries
-WHERE emp_no = 500001
+WHERE emp_no IN (500001,500002) 
 ;
 DELETE FROM dept_emp
-WHERE emp_no = 500001
+WHERE emp_no IN (500001,500002) 
 ;
 -- 6.'d009'부서의 관리자를 나로 변경해 주세요.
+UPDATE dept_manager
+SET to_date = DATE(NOW())
+WHERE dept_no = 'd009'
+	AND to_date > DATE(NOW());
+INSERT INTO dept_manager(dept_no, emp_no, from_date, to_date)
+VALUES('d009', 500000, DATE(NOW()) ,99990101)
+;
 -- 7. 오늘 날짜부로 나의 직책을 'Senior Engineer'로 변경해 주세요.
 UPDATE titles
-SET
-	title = 'Senior Engineer'
-WHERE
-	emp_no = 500000
-	AND DATE(NOW())
-;
+SET to_date = DATE(NOW())
+WHERE emp_no = 500000;
+INSERT INTO titles (
+	emp_no
+	,title
+	,from_date
+	,to_date
+)
+VALUES (
+	500000
+	,'Senior Engineer'
+	,DATE(NOW())
+	,99990101
+);
+
+
 -- 8. 최고 연봉 사원과 최저 연봉 사원의 사번과 이름을 출력해 주세요.
 SELECT
-	MAX(sal.salary)
-	,MIN(sal.salary)
-	,emp.emp_no
+	emp.emp_no
 	,emp.first_name
 FROM employees emp
 	JOIN salaries sal
-		ON emp.emp_no = sal.emp_no		
+		ON emp.emp_no = sal.emp_no
+		AND sal.to_date > DATE(NOW())		
+		AND sal.salary IN (                                           
+		(SELECT MAX(salary) FROM salaries WHERE to_date > DATE(NOW()))
+		,(SELECT MIN(salary) FROM salaries WHERE to_date > DATE(NOW()))	
+	)
 ;
+
+SELECT
+	emp.emp_no
+	,emp.first_name
+FROM employees emp
+	JOIN salaries sal
+		ON emp.emp_no = sal.emp_no
+		AND sal.to_date > DATE(NOW())	
+	JOIN ( SELECT 
+				MAX(salary) max_sal
+				,MIN(salary) min_sal
+			FROM salaries 
+			WHERE to_date > DATE(NOW())
+			) minmax_sal
+		ON sal.salary IN (minmax_sal.max_sal,minmax_sal.min_sal)
+;
+
 
 -- 9. 전체 사원의 평균 연봉을 출력해 주세요.
 SELECT
-	AVG(salary)
+	AVG(salary) avg_sal
 FROM salaries
-WHERE to_date >= NOW();
+WHERE to_date >= DATE(NOW());
 
 -- 10. 사번이 499,975인 사원의 지금까지 평균 연봉을 출력해 주세요.
 SELECT
 	emp_no
-	,AVG(salary)
+	,AVG(salary) avg_sal
 FROM salaries
 WHERE emp_no = 499975
 ;
 
-CREATE DATABASE users;
-USE users;
+-- 01
 CREATE TABLE users (
-	user_id			INT 				PRIMARY KEY AUTO_INCREMENT
-	,user_name 		VARCHAR(30) 	NOT NULL
-	,authflg			CHAR(1)			DEFAULT 0
-	,birth_day		DATE				NOT NULL
+	userid			INT 				PRIMARY KEY AUTO_INCREMENT
+	,username 		VARCHAR(30) 	NOT NULL
+	,authflg			CHAR(1)			DEFAULT '0'
+	,birthday		DATE				NOT NULL
 	,created_at 	DATETIME 		DEFAULT CURRENT_TIMESTAMP() COMMENT 'YYYY-MM-DD'
 );
 
+-- 02
 INSERT INTO users (
-	user_id
-	,user_name
+	username
 	,authflg
-	,birth_day
+	,birthday
 	,created_at
 )
 VALUES (
-	000001
-	,'그린'
+	'그린'
 	,0
 	,20240126
-	,20240311
+	,NOW()
 );
 
+-- 03
+UPDATE users
+SET 
+	username = '테스터'
+	,authflg = '1'
+	,birthday = 20070301
+WHERE userid = 1;
+	
+-- 04
+DELETE FROM users
+WHERE userid = 1;
 
+-- 05
+ALTER TABLE users ADD COLUMN addr VARCHAR(100) NOT NULL DEFAULT '-';
 
+-- 06
+DROP TABLE users;
 
-
-
-
-
-
-
-
-
+-- 07
+SELECT
+	usr.username
+	,usr.birthday
+	,rmg.rankname
+FROM uesrs usr
+	JOIN reankmanagement rmg
+		ON usr.userid = rmg.uesrid
+;
 
 
 
