aaea4de5b4222beacd0502d480e8bb524fade191
minsea1256 <minsea1256@gmail.com>
2024-04-25 17:41:01 +0900

    주석 추가

--- JavaScript/tng/41.js
+++ JavaScript/tng/41.js
@@ -6,14 +6,14 @@ myTimer(); //맨처음에 한번 실행
 
 function myTimer() {
     const date = new Date();
-    document.querySelector("h1").innerHTML = `현재 시각 ` + date.toLocaleTimeString();
+    document.querySelector("h1").innerHTML = `현재 시각 ` + date.toLocaleTimeString(); //시간 & 화면출력
 }
 function myStopFunction() {
     clearInterval(myInterval); // 중단
 }
-STOP.addEventListener('click', myStopFunction);
+STOP.addEventListener('click', myStopFunction); // 버튼연결
 
 function myStartFunction() {
     myInterval = setInterval(myTimer, 1000); // 재시작
 }
-START.addEventListener('click', myStartFunction);
+START.addEventListener('click', myStartFunction); //버튼연결
